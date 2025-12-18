4074c18efa3f7004e6e840c6af89d6aac0b37b12
quyenha38 <quyen.ha38@hcmut.edu.vn>
2025-12-18 13:17:28 +0700

    Webserver ver 1.0

---
+++ include/global.h
@@ -0,0 +1,7 @@
+#ifndef __GLOBAL_H__
+#define __GLOBAL_H__
+
+extern float glob_temperature;
+extern float glob_humidity;
+
+#endif
---
+++ src/global.cpp
@@ -0,0 +1,3 @@
+#include "global.h"
+float glob_temperature = 0;
+float glob_humidity = 0;
